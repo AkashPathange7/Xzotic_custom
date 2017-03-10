51e88992b4ecad16994d250c6af0e25b9f9587d2
As007Akash <akashpathange.aaa47@gmail.com>
2017-03-10 19:13:48 +0530

    Add: cpu governors CONSERVATIVEX & SMARTASS2
    Signed-off-by: As007Akash <akashpathange.aaa47@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -477,6 +477,12 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVEX)
+extern struct cpufreq_governor cpufreq_gov_conservativex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservativex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_smartass2)
 #endif
 
 /*********************************************************************
